b0ad528e52c803c5d9f8e8afec3ecea79e14acd8
Dan Cassidy <5643061+rux616@users.noreply.github.com>
2024-05-28 16:34:18 +1200

    revert changes to baseline 1.2.1

--- .version.ps1
+++ .version.ps1
@@ -1,15 +1,35 @@
+# Copyright 2023 Dan Cassidy
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+# define the version of the project
+
 # version info
-$version_major = 2
-$version_minor = 0
-$version_patch = 0
+$version_major = 1
+$version_minor = 2
+$version_patch = 1
 
 # prerelease info
-$version_prerelease = 1
+$version_prerelease = 0
 $is_pre = $false
 $is_rc = $false
 $is_beta = $false
-$is_alpha = $true
+$is_alpha = $false
 
 # build info
-$build_number = 356
+$build_number = 344
 $include_build_in_version = $false
--- data/MCM/Config/Portable Junk Recycler Mk 2/Portable Junk Recycler Mk 2.jsonnet
+++ data/MCM/Config/Portable Junk Recycler Mk 2/Portable Junk Recycler Mk 2.jsonnet
@@ -59,7 +59,7 @@ local threads_step = 1;
   local mod = {
     name: 'Portable Junk Recycler Mk 2',
     localized_name: '$PortableJunkRecyclerMk2',
-    version: '2.0.0-alpha.1',
+    version: '1.2.1',
     plugin_name: mod.name + '.esp',
     quest_form: mod.plugin_name + '|800',
     control_script: 'PortableJunkRecyclerMk2:PJRM2_ControlManager',
